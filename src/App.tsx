17b8d05cb8e71cc7a23782f386e2357364bda355
C5311821 <frank.he01@sap.com>
2020-10-01 14:29:25 +0800

    new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import * as React from "react";
 import {DatePicker} from "antd";
 import 'antd/dist/antd.css';
+import '../style/App.scss';
+import locale from 'antd/es/date-picker/locale/zh_CN';
+import * as moment from "moment";
 
-export class helloWorld extends React.Component{
+const Text = function(props){
+    return <div><span className="text">The current date is {props.date}</span></div>
+}
+
+export class helloWorld extends React.Component<any,any>{
     constructor(pros) {
         super(pros);
+        this.state={date:""};
+        this.onSelectDate = this.onSelectDate.bind(this);
+    }
+
+    onSelectDate(date: moment.MomentInput, dateString: string){
+        this.setState((state,prop)=>({date:dateString}));
     }
 
     render() {
-        return <DatePicker/>;
+        return (
+            <div>
+                <DatePicker locale={locale} onChange={this.onSelectDate}/>
+                <Text date={this.state.date}/>
+            </div>);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -76,6 +76,16 @@ module.exports = {
             {
                 test:/\.tsx?$/,
                 use:['ts-loader']
+            },
+            {
+                test:/\.scss$/,
+                use:[{
+                    loader: "style-loader"      // 将 JS 字符串生成为 style 节点
+                },{
+                    loader: "css-loader"        // 将 CSS 转化成 CommonJS 模块
+                },{
+                    loader: "sass-loader"       // 将 Sass 编译成 CSS
+                }]
             }
         ]
     },
