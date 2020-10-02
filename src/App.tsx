08a4ee1a34c11e53ab43744a92ec0aa4e70b6e43
C5311821 <frank.he01@sap.com>
2020-10-02 22:16:58 +0800

    new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import * as React from "react";
+import * as React from 'react';
 import '../style/App.scss';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import {Login} from "./Login";
+// import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {Login} from './Login';
 
-//React.Component<any,any>第一个参数是props的type，第二个参数是state的参数
-export class App extends React.Component<any,any>{
+/**
+ * React.Component<any,any>第一个参数是props的type，第二个参数是state的参数
+ */
+
+interface props{}
+interface state{}
+
+export class App extends React.Component<props, state> {
     constructor(pros) {
         super(pros);
     }
-    
-    render(){
+    render() {
         return(<Login/>);
     }
 }
