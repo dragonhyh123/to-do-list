36bc4b95731eac49f10d634fc70e8e98a697200e
C5311821 <frank.he01@sap.com>
2020-10-09 20:07:22 +0800

    add redux middleware

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,17 +1,27 @@
-import {Action, createStore, Reducer, Store, StoreEnhancer, applyMiddleware,Middleware} from 'redux';
+import {Action, createStore, Reducer, Store, StoreEnhancer, applyMiddleware,Middleware,PreloadedState} from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import {createLogger} from 'redux-logger';
 import reducer from '../reducers';
 
-export function configureStore() {
+interface initialState{login?:{userName:string,passWord:string}}
+
+function * helloSaga() {
+    console.log('Hello Sagas!');
+}
+
+export function configureStore(initialState?:PreloadedState<initialState>) {
     let middleware1:Middleware = store => next => action => {
             console.log('dispatching', action);
             let result = next(action);
             console.log('next state', store.getState());
             return result;
         };
+    const sagaMiddleware=createSagaMiddleware();
 
-    const enhancer = applyMiddleware(middleware1,thunkMiddleware);
-    const store = createStore(reducer,enhancer);
+    const enhancer = applyMiddleware(middleware1,thunkMiddleware,sagaMiddleware);
+    const store = createStore(reducer,initialState,enhancer);
+    sagaMiddleware.run(helloSaga);
 
     if ((module as any).hot) {
         // Enable Webpack hot module replacement for reducers
