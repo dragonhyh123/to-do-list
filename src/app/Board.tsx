afa0615e63f2f5b6e9a88c86e5c0f6879c2db82f
youhe <youhe@ebay.com>
2021-08-05 00:39:16 +0800

    new version

--- src/app/Board.tsx
+++ src/app/Board.tsx
@@ -6,7 +6,7 @@ import { match, history, location } from 'react-router-dom';
 import { MenuInfo } from 'rc-menu/lib/interface';
 
 const useState = React.useState;
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
 
 //webpack创建了一个全局变量process.env.NODE_ENV,用来区分不同的状态，同时可以在程序中区分程序状态
 // if(process.env.NODE_ENV==='development'){
@@ -32,7 +32,7 @@ interface boardState {
 
 function Title(props: titleProps) {
     return (
-        <div>
+        <div className='titles'>
             <h1 className="boardTitle">{`Welcome ${props.text}`}</h1>
         </div>
     );
@@ -55,7 +55,7 @@ function NavigationBar(props: titleBarProps) {
     }
 
     return (
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="inline" style={{ width: 256 }}>
+        <Menu theme="dark" onClick={handleClick} selectedKeys={[current]} mode="inline" style={{ width: 256 }}>
             <Menu.Item key="overview" icon={<MailOutlined />}>
                 OverView
             </Menu.Item>
@@ -72,14 +72,13 @@ export class Board extends React.Component<boardProps, boardState>{
     render() {
         return (
             <Layout>
-                {/* <Title text={this.props.match.params.text} /> */}
-                <Sider>
-                    <div className="logo" />
+                <Sider collapsible={true} width={256} style={{overflow: 'auto',height: '100vh', left: 0}}>
+                    <div className="logo"/>
                     <NavigationBar />
                 </Sider>
                 <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{ padding: 0 }}>
-                        
+                    <Header className="site-layout-background" style={{ margin: '24px 16px 0px 16px'}}>
+                        {/* <Title text={this.props.match.params.text}/> */}
                     </Header>
                     <Content
                         className="site-layout-background"
@@ -90,6 +89,7 @@ export class Board extends React.Component<boardProps, boardState>{
                         }}>
                         Content
                     </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Dragon</Footer>
                 </Layout>
             </Layout>)
     }
